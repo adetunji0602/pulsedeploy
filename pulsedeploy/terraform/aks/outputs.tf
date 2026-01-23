704b3870d88dbf1bb24bae34527afe7ab5fcd4e4
adetunji0602 <adejayiadetunji@gmail.com>
2026-01-23 03:36:39 +0100

    mainiting the new folder

--- pulsedeploy/terraform/aks/outputs.tf
+++ pulsedeploy/terraform/aks/outputs.tf
@@ -1,15 +1,12 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
 output "kube_config" {
-  description = "Kubeconfig to access AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive   = true
-}
-
-output "aks_cluster_id" {
-  description = "AKS cluster resource ID"
-  value       = azurerm_kubernetes_cluster.aks.id
-}
-
-output "log_analytics_workspace_id" {
-  description = "Log Analytics Workspace ID"
-  value       = azurerm_log_analytics_workspace.log_analytics.id
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
